8b4e1cdf3c93489543231fc8d767388524d8c108
dani <soudry.daniel99@gmail.com>
2020-02-03 16:07:54 +0100

    Update index.php

--- View/index.php
+++ View/index.php
@@ -2,7 +2,7 @@
 
 <h1>Accueil</h1>
 
-<h3>Vente de produits sa grande soebur</h3>
+<h3>Vente de produits</h3>
 
 <div class="info">
   <a href="../Controller/c_listeProduit.php"><button type="button" class="btn btn-primary">Liste des produits</button></a>
